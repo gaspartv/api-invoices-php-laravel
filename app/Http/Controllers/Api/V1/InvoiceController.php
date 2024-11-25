af1f11019e829f54f8a8306849aab452f0c18d11
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-11-24 22:47:49 -0300

    feat: add create, update and delete invoices

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Models\Invoice;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class InvoiceController extends Controller
 {
+    use HttpResponses;
+
     /**
      * Display a listing of the resource.
      */
@@ -18,19 +23,29 @@ class InvoiceController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'type' => 'required|max:1',
+            'paid' => 'required|numeric|between:0,1',
+            'payment_date' => 'nullable',
+            'value' => 'required|numeric|between:1,999999.99',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Data invalid', 422, $validator->errors());
+        }
+
+        $created = Invoice::create($validator->validated());
+
+        if ($created) {
+            return $this->response('Invoice created', 201, new InvoiceResource($created->load('user')));
+        }
+
+        return $this->error('Failed to create invoice', 400);
     }
 
     /**
@@ -42,26 +57,51 @@ class InvoiceController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
+     * @throws ValidationException
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Invoice $invoice)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'type' => 'required|max:1|in:' . implode(',', ['B', 'C', "P"]),
+            'paid' => 'required|numeric|between:0,1',
+            'payment_date' => 'nullable|date_format:Y-m-d H:i:s',
+            'value' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Data invalid', 422, $validator->errors());
+        }
+
+        $validated = $validator->validated();
+
+        $updated = $invoice->update([
+            'user_id' => $validated['user_id'],
+            'type' => $validated['type'],
+            'paid' => $validated['paid'],
+            'payment_date' => $validated['paid'] ? $validated['payment_date'] : null,
+            'value' => $validated['value'],
+        ]);
+
+        if ($updated) {
+            return $this->response('Invoice updated', 200, new InvoiceResource($invoice->load('user')));
+        }
+
+        return $this->error('Failed to update invoice', 400);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Invoice $invoice)
     {
-        //
+        $deleted = $invoice->delete();
+
+        if ($deleted) {
+            return $this->response('Invoice deleted', 200);
+        }
+
+        return $this->error('Failed to delete invoice', 400);
     }
 }
